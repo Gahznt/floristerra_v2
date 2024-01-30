7ac94b32fae18b9fd3c694d8eb8e71a7ae451a76
Gabriel Zanata <zanatag.gz@gmail.com>
2024-01-30 14:47:09 -0300

    comentando seed admin

--- v2/database/seeders/DatabaseSeeder.php
+++ v2/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CreateAdminUserSeed::class);
+        // $this->call(CreateAdminUserSeed::class);
         $this->call(CreateZanataUserSeed::class);
     }
 }
